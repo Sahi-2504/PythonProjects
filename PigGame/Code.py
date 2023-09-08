15acd1042f8fc5e7b315168485b44e5f34c033c5
Sahithi Oddiraju <106399295+Sahi-2504@users.noreply.github.com>
2023-09-08 17:35:13 -0400

    Add files via upload

---
+++ PigGame/Code.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  8 15:52:43 2023
+
+@author: sahit
+"""
+
+import random
+
+def roll():
+    min_value=1
+    max_value=6
+    roll=random.randint(min_value, max_value)
+    
+    return roll
+
+while True:
+    players=input("Enter the number of players(2-5): ")
+    if players.isdigit():
+        players= int(players)
+        if 2<=players<=5:
+            break
+        else:
+            print("Try Again")
+    else:
+      print("Invalid,Try Again")
+        
+        
+max_score = 50
+player_scores=[0 for _ in range(players)]
+print(player_scores)
+
+while max(player_scores) < max_score:
+    for player_idx in range(players):
+        print("\n Player",player_idx + 1 , "turn has just started")
+        print("Your total score",player_scores[player_idx],"\n")
+        current_score=0
+        while True:
+            
+            should_roll = input("Would you like to roll(y) ?")
+            if should_roll.lower() != "y":
+                break
+            else:
+                value= roll()
+                if value == 1:
+                    print("You rolled a 1! Turn Done!")
+                    current_score=0
+                    break
+                else:
+                    current_score =current_score+value
+                    print("You rolled a:",value)
+            print("Your Score is:",current_score)
+        
+    player_scores[player_idx] += current_score
+    
+max_score = max(player_scores)
+winning_idx=player_scores.index(max_score)
+print("Winner is",winning_idx+1)
